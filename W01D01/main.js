b1442f449253c76960d07adaf5ef88cec1353a3d
mabuobaid7 <93932080+mabuobaid7@users.noreply.github.com>
2021-11-09 18:29:46 +0200

    Add files via upload

---
+++ W01D01/main.js
@@ -0,0 +1,28 @@
+console.log(arrowTitle)
+/* START CODE UNDER THIS LINE */
+/*practies*/ 
+//(P1)
+//Q(1)   9+1*5=14
+//Q(2)   9%1=0
+//Q(3)   3*3=9 
+//Q(4)   4+48-5=5
+//(P2)   
+//(Q1) CONST@NAME  => CONST NAME 
+//(q2) conct 'age' = 10 => const age = 10
+// (Q3) كلمه محجوزه
+//(q4) space
+//const color = "red";
+//const positiveNumber = 2
+//const food  = mansaf
+//const PhoneNumber  = 0788886221
+//10 + "10"  = 1010
+//"10" + "10" = 1010
+//10 + 10 + "5" = 205
+//"Hello" + 1; = Hello 1
+//10" + 5 + 5; =1010
+//"20" - 10; =10
+//"9" - "5"; =4
+//"19" - "13" + 17; = 23
+//5 + 6 + "4" + 9 - 4 - 2;=1143
+//console.log () "mohammad"+"abu obaid"+"male"+"jordan
+// let a=(30*24)*(60*60)
